afba1d0b5d5ada497f1717782373946adfa7c3c3
Benjamin Youmans <youmansbenjamin@yahoo.com>
2022-06-23 17:43:05 -0400

    added arrays for password characters

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 // Assignment code here
-
+  //do you want uppercase letters
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  //do you want lowercase letters
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  //do you want numbers
+  var number = ["0","1","2","3","4","5","6","7","8","9"]
+  //do you want special characters
+  var specChar = ["!","@","#","$","%","&","*","(",")"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
-  //do you want uppercase letters
-  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //do you want lowercase letters
-  var lower = "abcdefghijklmnopqrstuvwxyz";
-  //do you want numbers
-  var number = "0123456789";
-  //do you want special characters
-  var characters = "!@#$&";
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
