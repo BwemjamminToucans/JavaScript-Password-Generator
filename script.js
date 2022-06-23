81d4fcc29dec430f4d2d0b042f9e95fb9cf223a5
Benjamin Youmans <youmansbenjamin@yahoo.com>
2022-06-23 17:57:00 -0400

    added userImput prompts

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Assignment code here
-  //do you want uppercase letters
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  //do you want lowercase letters
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  //do you want numbers
-  var number = ["0","1","2","3","4","5","6","7","8","9"]
-  //do you want special characters
-  var specChar = ["!","@","#","$","%","&","*","(",")"];
+//do you want uppercase letters
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//do you want lowercase letters
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var number = ["0","1","2","3","4","5","6","7","8","9"] //do you want numbers
+var specChar = ["!","@","#","$","%","&","*","(",")"]; //do you want special characters
+var userImput = prompt ("How many characters would you like your password to be? (Must be between 8-128 characters)")
+var userImput = prompt ("Do you want your password to contain Upper-Case Letters?")
+var userImput = prompt ("Do you want your password to contain Lower-Case Letters?")
+var userImput = prompt ("Do you want your password to contain Numbers?")
+var userImput = prompt ("Do you want your password to contain Special Characters?")
+
+  
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
